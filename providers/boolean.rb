f076d45aa05129718ebbb9df942a64b89265dcc8
alexgreco86 <alex.g@sweetiq.com>
2018-05-17 17:36:46 -0400

    PENG-2589 - Merging in latest release v2.1.0.  Took everything as is.

--- providers/boolean.rb
+++ providers/boolean.rb
@@ -5,24 +5,22 @@ def whyrun_supported?
   true
 end
 
-use_inline_resources
-
 # Set for now, without persisting
 action :set do
-  set_sebool(false)
+  sebool(false)
 end
 
 # Set and persist
 action :setpersist do
-  set_sebool(true)
+  sebool(true)
 end
 
-def set_sebool(persist = false)
-  persist_string = persist ? '-P ' : ''
-  new_value = new_resource.state ? 'on' : 'off'
-  e = execute "selinux-setbool-#{new_resource.name}-#{new_value}" do
-    command "/usr/sbin/setsebool #{persist_string} #{new_resource.name} #{new_value}"
-    not_if "/usr/sbin/getsebool #{new_resource.name} | grep '#{new_value}$' >/dev/null" unless new_resource.force
+def sebool(persist = false)
+  persist_string = persist ? '-P ' :  ''
+  new_value = new_resource.value ? 'on' : 'off'
+  execute "selinux-setbool-#{new_resource.name}-#{new_value}" do
+    command "setsebool #{persist_string} #{new_resource.name} #{new_value}"
+    not_if "getsebool #{new_resource.name} | grep '#{new_value}$' >/dev/null" unless new_resource.force
     only_if { use_selinux }
   end
 end
--- providers/permissive.rb
+++ providers/permissive.rb
@@ -5,22 +5,20 @@ def whyrun_supported?
   true
 end
 
-use_inline_resources
-
 # Create if doesn't exist, do not touch if port is already registered (even under different type)
 action :add do
-  e = execute "selinux-permissive-#{new_resource.name}-add" do
-    command "/usr/sbin/semanage permissive -a '#{new_resource.name}'"
-    not_if  "/usr/sbin/semanage permissive -l | grep  '^#{new_resource.name}$'"
+  execute "selinux-permissive-#{new_resource.name}-add" do
+    command "semanage permissive -a '#{new_resource.name}'"
+    not_if  "semanage permissive -l | grep  '^#{new_resource.name}$'"
     only_if { use_selinux }
   end
 end
 
 # Delete if exists
 action :delete do
-  e = execute "selinux-port-#{new_resource.name}-delete" do
-    command "/usr/sbin/semanage permissive -d '#{new_resource.name}'"
-    not_if  "/usr/sbin/semanage permissive -l | grep  '^#{new_resource.name}$'"
+  execute "selinux-port-#{new_resource.name}-delete" do
+    command "semanage permissive -d '#{new_resource.name}'"
+    not_if  "semanage permissive -l | grep  '^#{new_resource.name}$'"
     only_if { use_selinux }
   end
 end
